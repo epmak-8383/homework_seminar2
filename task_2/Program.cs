6bea4bb7b19d6f847bc9834465b2242bc81fb126
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-07 00:45:00 +0300

    вторая задача

---
+++ task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("введите число: ");
+int n = int.Parse(Console.ReadLine());
+int result_a = n%10;
+
+if (n < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    if (n < 1000)
+    {
+        Console.WriteLine(result_a);
+    }
+    else
+    {
+        while (n >= 1000)
+        {
+            n = n / 10;
+        }
+        int result_b = n%10;
+        Console.WriteLine(result_b);
+    }
+}
