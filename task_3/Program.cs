4a3b0d4243509917bef81a2b12f128a9fee4a098
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-07 00:51:57 +0300

    третье задание 2 семинара

---
+++ task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число от 1 до 7 включительно: ");
+int n = int.Parse(Console.ReadLine());
+if (n > 5)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("рабочий");
+}
